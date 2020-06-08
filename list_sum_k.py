26752fe26fed37a141f63461b4322a83bfdf7f19
alla <andreanndrade10@gmail.com>
2020-06-07 22:12:53 -0300

    first commit in python algo

---
+++ list_sum_k.py
@@ -0,0 +1,22 @@
+'''
+This program prints the elements that summed is equal 'k'
+'''
+import random
+
+def check_list_k(list, k):
+    for i in list:
+        for j in list:
+            if j+i==k:
+                print(i,j)
+
+if __name__ == "__main__":
+    random_list=[]
+    #generate random list with integers between -10 and 10
+    for i in range(10):
+        random_list.append(random.randint(-10,10))
+    #generate random number between -10 and 10
+    k = random.randint(-10,10)
+    print('List: {}'.format(random_list))
+    print('K: {} \n'.format(k))
+    check_list_k(random_list, k)
+    
