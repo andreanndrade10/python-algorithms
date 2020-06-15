f92becead2cb3d03f0a3c3d5c2e93fa7f2f292aa
Andre Luiz Lourenco de Andrade <andre.andrade@ish.com.br>
2020-06-15 11:18:54 -0300

    add sumMax and sumMin function in mini_max_sum.py

--- hackerrank/mini_max_sum.py
+++ hackerrank/mini_max_sum.py
@@ -3,12 +3,33 @@ Given five positive integers, find the minimum and maximum values that can be ca
 by summing exactly four of the five integers. Then print the respective minimum and
 maximum values as a single line of two space-separated long integers. 
 '''
+import random
 
 # Complete the miniMaxSum function below.
 def miniMaxSum(arr):
-    
+    arrMin = arr
+    arrMax = arr
+    print(sumMin(arrMin))
+    print(sumMax(arrMax))
+   
+def sumMax(array):
+    array.remove(min(array))
+    sumMax=0
+    for i in array:
+        sumMax+=i
+    return sumMax
 
+def sumMin(array):
+    array.remove(max(array))
+    sumMin=0
+    for i in array:
+        sumMin+=i
+    return sumMin
 
 if __name__ == '__main__':
-    arr = list(map(int, input().rstrip().split()))
-    miniMaxSum(arr)
+    #generate random array and calling the miniMaxSum function
+    #arr = []
+    #for i in range(5):
+    #    arr.append(random.randint(0,50))
+    #print(arr)
+    miniMaxSum([1,2,3,4,5])
